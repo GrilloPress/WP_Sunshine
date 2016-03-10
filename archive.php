c50173f6aeada42a62af44099d212da6b3755700
GrilloPress <grillopress@gmail.com>
2016-03-10 23:03:50 +0000

    add STH WP theme boilerplate

--- archive.php
+++ archive.php
@@ -1,11 +1,7 @@
 <?php
 /**
- * The main template file.
+ * The template for displaying archive pages.
  *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
  * Learn more: http://codex.wordpress.org/Template_Hierarchy
  *
  * @package sth
@@ -19,16 +15,18 @@ get_header(); ?>
 
 		<?php if ( have_posts() ) : ?>
 
-			<?php if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-			<?php endif; ?>
+			<header class="page-header">
+				<?php
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="taxonomy-description">', '</div>' );
+				?>
+			</header><!-- .page-header -->
 
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php
+
 					/*
 					 * Include the Post-Format-specific template for the content.
 					 * If you want to override this in a child theme, then include a file
@@ -52,7 +50,9 @@ get_header(); ?>
       <aside class="col-md-4 col-sm-4">
         <?php get_sidebar(); ?>
       </aside>
+      
     </div>
 	</div><!-- #primary -->
+
 
 <?php get_footer(); ?>
---
+++ readme.md
@@ -0,0 +1,13 @@
+# North of England Breech Birth Conference 2017
+
+Things to set up:
+
+- Sass and compass for styles
+- Eventbrite event/plugin
+- Eventbrite to Google Sheet connector
+- Add piklist theme engine
+- Update template parts structure to views folder and plugin/functions.php stuff to lib folder
+
+## branding
+
+color: #18bc9c
