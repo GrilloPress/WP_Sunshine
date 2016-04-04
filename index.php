3d4058677a2a43357ff8c4a167d3fc00e2d3062c
GrilloPress <grillopress@gmail.com>
2016-04-04 21:48:30 +0000

    return to classic sth font

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ get_header(); ?>
 		<?php if ( have_posts() ) : ?>
 
 			<?php if ( is_home() && ! is_front_page() ) : ?>
-				<header>
+				<header class="page-header archive-header">
 					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
 				</header>
 			<?php endif; ?>
